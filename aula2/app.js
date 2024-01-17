e27f5f5894bc9a0247f7666dd50d230f1ad38961
Santiago Pereira <santiagopereira@MacBook-Pro-de-Santiago.local>
2024-01-17 19:58:48 +0000

    aula nº2 de do módulo programação 2

---
+++ aula2/app.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const menua = (x) => {
+
+    let num = 20;
+
+    return x + num;
+}
+
+let int = 10;
+
+console.log(menua(int));
+
+const addNumbers = (a, b) => {
+
+    return a + b;
+}
+
+let a = 1;
+let b = 2;
+
+console.log(addNumbers(a, b))   
+
+
